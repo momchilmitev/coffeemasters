cd17a3de5aad36292b1e88b0cd8ef0880fe8de79
momchilmitev <momchilmitev87@gmail.com>
2023-09-20 22:14:56 +0200

    added javascript entry point file(app.js) and some helper functions

---
+++ app.js
@@ -0,0 +1,6 @@
+const $ = () => document.querySelector.call(this, arguments);
+const $$ = () => document.querySelectorAll.call(this, arguments);
+HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
+HTMLElement.prototype.on = (a, b) => this.addEventListener(a, b);
+HTMLElement.prototype.$ = (s) => this.querySelector(s);
+HTMLElement.prototype.$$ = (s) => this.querySelectorAll(s);
