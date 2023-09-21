dd9abb6cc8dd54be461dca1e539af9d61844309b
momchilmitev <momchilmitev87@gmail.com>
2023-09-21 21:11:59 +0200

    implemented the init router method and initialized the router

--- app.js
+++ app.js
@@ -9,4 +9,5 @@ app.router = Router;
 
 window.addEventListener('DOMContentLoaded', (e) => {
   loadMenuData();
+  app.router.init();
 });
--- services/Router.js
+++ services/Router.js
@@ -1,6 +1,16 @@
 const Router = {
-  init: () => {},
-  go: (path, addToHistory = true) => {},
+  init: () => {
+    document.querySelectorAll('a.navlink').forEach((a) => {
+      a.addEventListener('click', (e) => {
+        e.preventDefault();
+        const path = e.target.getAttribute('href');
+        Router.go(path);
+      });
+    });
+  },
+  go: (path, addToHistory = true) => {
+    console.log(path);
+  },
 };
 
 export default Router;
