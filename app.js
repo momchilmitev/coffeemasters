33f39db264d3a058ed9718d5f81bdcf642f584cd
momchilmitev <momchilmitev87@gmail.com>
2023-09-21 20:59:02 +0200

    added router service and made it global

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadMenuData } from './services/Menu.js';
+import Router from './services/Router.js';
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 window.addEventListener('DOMContentLoaded', (e) => {
   loadMenuData();
---
+++ services/Router.js
@@ -0,0 +1,6 @@
+const Router = {
+  init: () => {},
+  go: (path, addToHistory = true) => {},
+};
+
+export default Router;
