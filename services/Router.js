acd278292d410c0399e9b20fa4ef032fa82b91ec
momchilmitev <momchilmitev87@gmail.com>
2023-09-21 21:45:25 +0200

    implemented the router go method

--- services/Router.js
+++ services/Router.js
@@ -7,9 +7,45 @@ const Router = {
         Router.go(path);
       });
     });
+
+    // Check the initial path
+    Router.go(location.pathname);
   },
   go: (path, addToHistory = true) => {
     console.log(path);
+
+    if (addToHistory) {
+      history.pushState({ path }, null, path);
+    }
+
+    let component = null;
+
+    switch (path) {
+      case '/':
+        component = document.createElement('h1');
+        component.textContent = 'Hi from the HOME page.';
+        break;
+      case '/order':
+        component = document.createElement('h1');
+        component.textContent = 'Hi from your cart.';
+        break;
+      default:
+        if (path.startsWith('/details-')) {
+          component = document.createElement('h1');
+          component.textContent = 'Details page';
+          const param = path.substring(path.lastIndexOf('-') + 1);
+          component.dataset.id = param;
+        }
+        component = document.createElement('h1');
+        component.textContent = 'Not found';
+        break;
+    }
+
+    const outlet = document.querySelector('main');
+    outlet.innerHTML = '';
+    outlet.appendChild(component);
+    window.scrollX = 0;
+    window.scrollY = 0;
   },
 };
 
