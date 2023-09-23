feea029e4dba1b88a147e1abcbd889391210a616
momchilmitev <momchilmitev87@gmail.com>
2023-09-23 07:34:49 +0200

    made the store proxy and added set trap

--- services/Store.js
+++ services/Store.js
@@ -3,4 +3,18 @@ const Store = {
   cart: [],
 };
 
-export default Store;
+const proxiedStore = new Proxy(Store, {
+  set(target, prop, value) {
+    target[prop] = value;
+
+    if (prop == 'menu') {
+      dispatchEvent(new Event('appmenuchange'));
+    }
+
+    if (prop == 'cart') {
+      dispatchEvent(new Event('appcartchange'));
+    }
+  },
+});
+
+export default proxiedStore;
