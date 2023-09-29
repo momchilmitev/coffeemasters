fc75283165baf62e69d03f6f88e94c35f648aef1
momchilmitev <momchilmitev87@gmail.com>
2023-09-29 22:30:01 +0200

    added some other caching strategies

--- serviceworker.js
+++ serviceworker.js
@@ -33,6 +33,7 @@ self.addEventListener('install', async (event) => {
   );
 });
 
+// Cache first strategy
 self.addEventListener('fetch', async (event) => {
   event.respondWith(
     caches.open('assets').then((cache) => {
@@ -46,3 +47,36 @@ self.addEventListener('fetch', async (event) => {
     }),
   );
 });
+
+// Network first strategy
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     fetch(event.request) // I go to the network ALWAYS
+//       .catch((error) => {
+//         // if the network is down, I go to the cache
+//         return caches.open('assets').then((cache) => {
+//           return cache.match(request);
+//         });
+//       }),
+//   );
+// });
+
+// State while revalidate strategy
+// self.addEventListener('fetch', event => {
+//    event.respondWith(
+//        caches.match(event.request)
+//            .then( response => {
+//                // Even if the response is in the cache, we fetch it
+//                // and update the cache for future usage
+//                const fetchPromise = fetch(event.request).then(
+//                     networkResponse => {
+//                        caches.open("assets").then( cache => {
+//                            cache.put(event.request, networkResponse.clone());
+//                            return networkResponse;
+//                        });
+//                    });
+//                // We use the currently cached version if it's there
+//                return response || fetchPromise; // cached or a network fetch
+//            })
+//        );
+//    });
